208c29a2d212b485486376e111a46063d74db55f
allenjosh1983 <138170235+allenjosh1983@users.noreply.github.com>
2023-10-28 16:21:44 -0500

    test full constructor

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -15,7 +15,7 @@ public class Employer {
 
     public Employer(String Value) {
         this();
-        this.value = value;
+        this.value = Value;
     }
 
     // Custom toString, equals, and hashCode methods:
@@ -26,16 +26,16 @@ public class Employer {
     }
 
     @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+    public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Employer employer = (Employer) o;
-        return getId() == employer.getId();
+        return id == employer.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId());
+        return Objects.hash(id);
     }
 
     // Getters and Setters:
